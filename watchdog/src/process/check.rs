65bbd30c7e84b08bca4fa279d14f1420ec543b42
MwlLj <731025894@qq.com>
2019-04-22 14:03:08 +0800

    开启线程监听进程退出

--- watchdog/src/process/check.rs
+++ watchdog/src/process/check.rs
@@ -31,81 +31,99 @@ impl CCheck {
         // Arc::get_mut(&mut self.system).unwrap().refresh_all();
         self.system.refresh_all();
         for item in &(*self.processes) {
-            #[cfg(target_os="windows")]
-            {
-                if self.system.get_process_by_name(&item.name).len() == 0 {
-                    if item.isAuto == true {
-                        if let Ok(_) = Command::new(&item.name)
-                        .args(&item.args)
-                        .env("PATH", &item.directory)
-                        .current_dir(&item.directory)
-                        .spawn() {
-                            println!("{} start success", &item.name);
-                        }
+            if self.system.get_process_by_name(&item.name).len() == 0 {
+                if item.isAuto == true {
+                    if let Ok(mut child) = Command::new(&item.name)
+                    .args(&item.args)
+                    .env("PATH", &item.directory)
+                    .current_dir(&item.directory)
+                    .spawn() {
+                        println!("{} start success", &item.name);
+                        thread::spawn(move || {
+                            child.wait();
+                        });
                     }
                 }
             }
-            #[cfg(target_os="linux")]
-            {
-                let ps = self.system.get_process_by_name(&item.name);
-                for it in ps {
-                    let pid = it.pid() as i32;
-                    let mut dir = String::new();
-                    dir.push_str("/proc/");
-                    dir.push_str(&pid.to_string());
-                    dir.push_str("/status");
-                    if let Ok(f) = File::open(dir) {
-                        let mut buf = BufReader::new(f);
-                        for line in buf.lines() {
-                            let line = line.unwrap();
-                            let v: Vec<&str> = line.split(":").collect();
-                            if v[0] == "State" {
-                                if v[1].contains("Z") {
-                                    if it.kill(Signal::Child) {
-                                        if item.isAuto == true {
-                                            if let Ok(_) = Command::new(&item.name)
-                                            .args(&item.args)
-                                            .env("PATH", &item.directory)
-                                            .current_dir(&item.directory)
-                                            .spawn() {
-                                                println!("{} start success", &item.name);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    } else {
-                        println!("open error");
-                    }
-                    // let status = it.status();
-                    // println!("{:?}", status);
-                    // if status == ProcessStatus::Zombie {
-                    //     if it.kill(Signal::Child) {
-                    //         if item.isAuto == true {
-                    //             if let Ok(_) = Command::new(&item.name)
-                    //             .args(&item.args)
-                    //             .env("PATH", &item.directory)
-                    //             .current_dir(&item.directory)
-                    //             .spawn() {
-                    //                 println!("{} start success", &item.name);
-                    //             }
-                    //         }
-                    //     }
-                    // }
-                }
-                if self.system.get_process_by_name(&item.name).len() == 0 {
-                    if item.isAuto == true {
-                        if let Ok(_) = Command::new(&item.name)
-                        .args(&item.args)
-                        .env("PATH", &item.directory)
-                        .current_dir(&item.directory)
-                        .spawn() {
-                            println!("{} start success", &item.name);
-                        }
-                    }
-                }
-            }
+            
+            // #[cfg(target_os="windows")]
+            // {
+            //     if self.system.get_process_by_name(&item.name).len() == 0 {
+            //         if item.isAuto == true {
+            //             if let Ok(mut child) = Command::new(&item.name)
+            //             .args(&item.args)
+            //             .env("PATH", &item.directory)
+            //             .current_dir(&item.directory)
+            //             .spawn() {
+            //                 println!("{} start success", &item.name);
+            //                 thread::spawn(move || {
+            //                     child.wait();
+            //                 });
+            //             }
+            //         }
+            //     }
+            // }
+            // #[cfg(target_os="linux")]
+            // {
+            //     let ps = self.system.get_process_by_name(&item.name);
+            //     for it in ps {
+            //         let pid = it.pid() as i32;
+            //         let mut dir = String::new();
+            //         dir.push_str("/proc/");
+            //         dir.push_str(&pid.to_string());
+            //         dir.push_str("/status");
+            //         if let Ok(f) = File::open(dir) {
+            //             let mut buf = BufReader::new(f);
+            //             for line in buf.lines() {
+            //                 let line = line.unwrap();
+            //                 let v: Vec<&str> = line.split(":").collect();
+            //                 if v[0] == "State" {
+            //                     if v[1].contains("Z") {
+            //                         if it.kill(Signal::Child) {
+            //                             if item.isAuto == true {
+            //                                 if let Ok(_) = Command::new(&item.name)
+            //                                 .args(&item.args)
+            //                                 .env("PATH", &item.directory)
+            //                                 .current_dir(&item.directory)
+            //                                 .spawn() {
+            //                                     println!("{} start success", &item.name);
+            //                                 }
+            //                             }
+            //                         }
+            //                     }
+            //                 }
+            //             }
+            //         } else {
+            //             println!("open error");
+            //         }
+            //         // let status = it.status();
+            //         // println!("{:?}", status);
+            //         // if status == ProcessStatus::Zombie {
+            //         //     if it.kill(Signal::Child) {
+            //         //         if item.isAuto == true {
+            //         //             if let Ok(_) = Command::new(&item.name)
+            //         //             .args(&item.args)
+            //         //             .env("PATH", &item.directory)
+            //         //             .current_dir(&item.directory)
+            //         //             .spawn() {
+            //         //                 println!("{} start success", &item.name);
+            //         //             }
+            //         //         }
+            //         //     }
+            //         // }
+            //     }
+            //     if self.system.get_process_by_name(&item.name).len() == 0 {
+            //         if item.isAuto == true {
+            //             if let Ok(_) = Command::new(&item.name)
+            //             .args(&item.args)
+            //             .env("PATH", &item.directory)
+            //             .current_dir(&item.directory)
+            //             .spawn() {
+            //                 println!("{} start success", &item.name);
+            //             }
+            //         }
+            //     }
+            // }
         }
     }
 
