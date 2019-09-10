5e40a58dab2cc0ff6b4fb40e2af621c9692b56a5
MwlLj <731025894@qq.com>
2019-09-10 20:26:49 +0800

    修改进程匹配规则

--- watchdog/src/process/control.rs
+++ watchdog/src/process/control.rs
@@ -371,9 +371,9 @@ impl CControl {
         system.refresh_all();
         let processes = system.get_process_by_name(name);
         for process in processes {
-            // println!("process cmd: {:?}\nargs: {:?}\nprocess exe: {:?}, dir: {:?}", &process.cmd()[1..].to_vec(), &args, process.cmd(), Path::new(dir));
+            // println!("process cmd: {:?}\nargs: {:?}\nprocess exe: {:?}, dir: {:?}", &process.cmd()[1..].to_vec(), &args, process.root(), Path::new(dir));
             if &process.cmd()[1..].to_vec() == args
-            && process.cwd() == Path::new(dir) {
+            && process.root() == Path::new(dir) {
                 println!("process starting ..., kill");
                 process.kill(sysinfo::Signal::Kill);
             }
