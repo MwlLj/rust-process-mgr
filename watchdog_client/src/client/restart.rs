157f5427c7e36d87f9a70f2c3c526cbaa9a5b7d7
MwlLj <731025894@qq.com>
2019-09-26 14:07:04 +0800

    修改http client

--- watchdog_client/src/client/restart.rs
+++ watchdog_client/src/client/restart.rs
@@ -1,4 +1,5 @@
-use reqwest::Client;
+// use reqwest::Client;
+use easy_http_request::DefaultHttpRequest;
 
 const restart_all_url: &str = "/api/restart/all";
 
@@ -7,11 +8,18 @@ pub fn restart_all(addr: &str) -> Result<(), &str> {
     url.push_str("http://");
     url.push_str(addr);
     url.push_str(restart_all_url);
-    let client = Client::new();
-    let response = match client.put(&url).send() {
+    // let client = Client::new();
+    let client = match DefaultHttpRequest::put_from_url_str(&url) {
+        Ok(c) => c,
+        Err(err) => {
+            println!("err: {:?}", err);
+            return Err("craete client error");
+        }
+    };
+    let response = match client.send() {
         Ok(r) => r,
         Err(err) => {
-            println!("err: {}", err);
+            println!("err: {:?}", err);
             return Err("send put request error");
         }
     };
--- watchdog_client/src/client/stop.rs
+++ watchdog_client/src/client/stop.rs
@@ -1,4 +1,5 @@
-use reqwest::Client;
+// use reqwest::Client;
+use easy_http_request::DefaultHttpRequest;
 
 const stop_all_url: &str = "/api/stop/all";
 
@@ -7,11 +8,17 @@ pub fn stop_all(addr: &str) -> Result<(), &str> {
     url.push_str("http://");
     url.push_str(addr);
     url.push_str(stop_all_url);
-    let client = Client::new();
-    let response = match client.delete(&url).send() {
+    let client = match DefaultHttpRequest::delete_from_url_str(&url) {
+        Ok(c) => c,
+        Err(err) => {
+            println!("err: {:?}", err);
+            return Err("craete client error");
+        }
+    };
+    let response = match client.send() {
         Ok(r) => r,
         Err(err) => {
-            println!("err: {}", err);
+            println!("err: {:?}", err);
             return Err("send delete request error");
         }
     };
