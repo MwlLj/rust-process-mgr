57fac32faafd03f9f398817f251010972d3fbcc1
MwlLj <731025894@qq.com>
2020-01-13 16:09:09 +0800

    update

--- watchdog_client/src/client/restart.rs
+++ watchdog_client/src/client/restart.rs
@@ -1,7 +1,8 @@
 // use reqwest::Client;
 // use easy_http_request::DefaultHttpRequest;
 use http_req::{request::Request, request::Method, uri::Uri};
-use urldecode;
+// use urldecode;
+use urlencoding;
 
 const header_name: &str = "name";
 const restart_all_url: &str = "/api/restart/all";
@@ -59,10 +60,10 @@ pub fn restart_by_alias<'a>(addr: &'a str, name: &str) -> Result<(), &'a str> {
     url.push_str("http://");
     url.push_str(addr);
     url.push_str(restart_by_alias_url);
-    let url = urldecode::decode(url);
+    // let url = urldecode::decode(url);
     let uri: Uri = url.parse().unwrap();
     let mut writer = Vec::new();
-    let n = urldecode::decode(String::from(name));
+    let n = urlencoding::encode(name);
     let response = match Request::new(&uri).method(Method::PUT)
     .header(header_name, &n)
     .send(&mut writer) {
